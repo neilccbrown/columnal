0880cdb50b6d86333282664a7ec3b6fbab8ba0af
Neil Brown <neil@twistedsquare.com>
2022-10-19 19:08:33 +0100

    Fixed the debug output.

--- tests-gui-base/src/testFixtures/java/test/gui/trait/ScrollToTrait.java
+++ tests-gui-base/src/testFixtures/java/test/gui/trait/ScrollToTrait.java
@@ -204,8 +204,8 @@ public interface ScrollToTrait extends FxRobotInterface, FocusOwnerTrait, QueryT
         }));
         if (usingMenu)
         {
-            Log.debug("Bounds for id-menu-view off-thread " + bounds("#id-menu-view").query());
-            Log.debug("Bounds for id-menu-view on-thread " + TFXUtil.fx((() -> bounds("#id-menu-view").query())));
+            Log.debug("Bounds for id-menu-view off-thread " + bounds(lookup("#id-menu-view").<Node>query()).query());
+            Log.debug("Bounds for id-menu-view on-thread " + TFXUtil.fx((() -> bounds("#id-menu-view").<Node>query())));
             Log.debug("Point for id-menu-view off-thread " + pointOfVisibleNode("#id-menu-view").query());
             Log.debug("Point for id-menu-view on-thread " + TFXUtil.fx((() -> pointOfVisibleNode("#id-menu-view").query())));
             clickOn("#id-menu-view").clickOn(".id-menu-view-goto-row");
